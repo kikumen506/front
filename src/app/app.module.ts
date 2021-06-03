29f353959a857804384e7ff3af9b250edf8c6f6a
kike gg <egonzalez@telynet.com>
2021-06-03 17:21:17 +0200

    start front

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BillsComponent } from './components/bills/bills.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BillsComponent
   ],
   imports: [
     BrowserModule
